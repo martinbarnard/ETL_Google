7c6fe9339e12a1183fb953fee7349829c2bcdcba
Martin Barnard <barnard.martin@gmail.com>
2018-03-17 18:52:48 +0000

    peppy

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 setup(name='ELT_Google',
-    version='0.1',
-    description='Extract Transform and Load Test',
-    py_modules=[''],
-    url='tbd',
-    author='martin barnard',
-    author_email='barnard.martin@gmail.com',
-    license='Public Domain',
-    packages=[''],
-    entry_points='''
-        [console_scripts]
-        etl=src.app:main
-    '''
-    )
+      version='0.1',
+      description='Extract Transform and Load Test',
+      py_modules=[''],
+      url='tbd',
+      author='martin barnard',
+      author_email='barnard.martin@gmail.com',
+      license='Public Domain',
+      packages=[''],
+      entry_points='''
+          [console_scripts]
+          etl=src.app:main
+      '''
+      )
