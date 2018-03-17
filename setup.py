02c3f6701008a63fb639a4800571c7605737f0ad
Martin Barnard <barnard.martin@gmail.com>
2018-03-17 19:08:31 +0000

    import bloody import

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
-setup(name='ELT_Google',
-      version='0.1',
-      description='Extract Transform and Load Test',
-      py_modules=['app'],
-      url='tbd',
-      author='martin barnard',
-      author_email='barnard.martin@gmail.com',
-      license='Public Domain',
-      packages=[''],
-      entry_points='''
-          [console_scripts]
-          etl=src.app:main
-      '''
-      )
+setup(
+    name='ELT_Google',
+    version='0.1',
+    setup_requires=['pytest'],
+    description='Extract Transform and Load Test',
+    py_modules=['app'],
+    url='tbd',
+    author='martin barnard',
+    author_email='barnard.martin@gmail.com',
+    license='Public Domain',
+    packages=[''],
+    entry_points='''
+      [console_scripts]
+      etl=src.app:main
+    '''
+)
