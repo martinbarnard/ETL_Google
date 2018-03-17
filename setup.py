3b3a684fb2a0fed6d7a444d909811d2e54330b7c
Martin Barnard <barnard.martin@gmail.com>
2018-03-17 19:00:41 +0000

    working on CI tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='ELT_Google',
       version='0.1',
       description='Extract Transform and Load Test',
-      py_modules=[''],
+      py_modules=['app'],
       url='tbd',
       author='martin barnard',
       author_email='barnard.martin@gmail.com',
